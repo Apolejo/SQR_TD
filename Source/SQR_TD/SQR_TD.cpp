551cc667ecb47cf6b17e9cd4747686c9d0514e79
Alejandro Apolinar <Apolejo1206@hotmail.com>
2025-10-26 12:18:37 -0500

    Engine first files

---
+++ Source/SQR_TD/SQR_TD.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SQR_TD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SQR_TD, "SQR_TD" );
---
+++ Source/SQR_TDEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SQR_TDEditorTarget : TargetRules
+{
+	public SQR_TDEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		ExtraModuleNames.Add("SQR_TD");
+	}
+}
